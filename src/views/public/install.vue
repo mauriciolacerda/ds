df7a238c818ba5cf0bbc3c90cb7056a540d158ff
mauriciolacerda <mauricioalacerda@gmail.com>
2020-05-08 15:07:31 -0300

    ajuste na instalação

--- src/views/public/install.vue
+++ src/views/public/install.vue
@@ -158,7 +158,7 @@
         this.e1 = 4
       }
       if (this.$route.query.shop && this.$route.query.hmac && this.$route.query.timestamp && !this.$route.query.state && !this.$route.query.code) {
-          this.getShopify (this.$route.query.shop)
+        this.getShopify(this.$route.query.shop)
       }
       if (this.$route.query.shop && this.$route.query.hmac && this.$route.query.code && this.$route.query.state) {
         this.e1 = 2
